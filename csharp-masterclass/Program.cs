6889dc64bfa991301aab1b87797e3e31b6e5c985
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-12-05 20:51:08 -0300

    just doing basic datatypes coding

--- csharp-masterclass/Program.cs
+++ csharp-masterclass/Program.cs
@@ -8,11 +8,19 @@ namespace csharp_masterclass
 {
     internal class Program
     {
+        static int age = 18;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            if(age < 18)
+            {
+                Console.WriteLine("He/she is not an adult!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("He/she is an adult!");
+                Console.ReadLine();
+            }
         }
     }
 }

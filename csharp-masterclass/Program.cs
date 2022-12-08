b7ad2bcc7810e3ef7855cbebf05d24f51185986f
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-12-07 21:18:29 -0300

    constants

--- csharp-masterclass/Program.cs
+++ csharp-masterclass/Program.cs
@@ -13,18 +13,8 @@ namespace csharp_masterclass
         static void Main(string[] args)
         {
 
-            byte bVar = 255;
-            sbyte sVar = -128;
-            int iVar = 2147483647;
-            short stVar = 32767;
-            float fVar = 3.402823e38f;
-            double dVar = 130823.232;
-            char cVar = 'c';
-            bool blVar = false;
-            decimal dlVar = 1028m;
+            const string neverChanges = "HIII";
 
-            string phrase1 = "I control text", phrase2 = "23232";
-            int phrase2AsInt = int.Parse(phrase2);
 
             Console.ReadKey();
         }

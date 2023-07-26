fbc6421a50b275c97bafa2893f45e8a758d3d939
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-07-26 00:06:58 -0300

    hash table challenge

---
+++ csharp-masterclass/HashTableChallenge.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_masterclass
+{
+    internal class HashTableChallenge
+    {
+        public HashTableChallenge() {
+            Students[] students = { 
+                new Students(1, "Marcello"),
+                new Students(2, "Pedro"),
+                new Students(3, "Lucas"),
+                new Students(4, "Paulo"),
+                new Students(1, "Paulo"),
+            };
+
+            Hashtable SchoolHashTable = new Hashtable();
+
+            foreach (Students student in students)
+            {
+                if (SchoolHashTable.ContainsKey(student.Id))
+                {
+                    Console.WriteLine($"Sorry, A student with the Id {student.Id} already registered.");
+                }
+                else
+                {
+                    SchoolHashTable.Add(student.Id, student);
+                    Console.WriteLine($"Student with the Id {student.Id} was added.");
+
+                }
+            }
+
+
+            foreach(Students student in SchoolHashTable.Values)
+            {
+                Console.WriteLine(student.Id);
+                Console.WriteLine(student.Name);
+            }
+
+
+
+        }
+    }
+}
--- csharp-masterclass/Program.cs
+++ csharp-masterclass/Program.cs
@@ -13,34 +13,9 @@ namespace csharp_masterclass
 
         static void Main(string[] args)
         {
-            Hashtable ht = new Hashtable();
 
-            Students newStudents1 = new Students(1, "Marcello");
-            Students newStudents2 = new Students(2, "Marcello");
-            Students newStudents3 = new Students(3, "Marcello");
-            Students newStudents4 = new Students(4, "Marcello");
-
-            ht.Add(newStudents1.Id, newStudents1);
-            ht.Add(newStudents2.Id, newStudents2);
-            ht.Add(newStudents3.Id, newStudents3);
-            ht.Add(newStudents4.Id, newStudents4);
-
-            foreach(DictionaryEntry entry in ht)
-            {
-                Students temp = (Students)entry.Value;
-
-                Console.WriteLine(temp.Id);
-                Console.WriteLine(temp.Name);
-            }
-
-
-            foreach(Students value in ht.Values)
-            {
-                Console.WriteLine(value.Id);
-                Console.WriteLine(value.Name);
-            }
-
-
+            //HashTable Challenge
+            //new HashTableChallenge();
 
             Console.ReadLine();
         }
